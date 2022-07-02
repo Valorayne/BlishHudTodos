f1ef010d578d56cd55ff676943e205392d8d9161
Valorayne <kanu.privat@gmail.com>
2022-07-02 22:09:17 +0200

    Adding the first TodoEntries

---
+++ Source/Components/TodoEntry.cs
@@ -0,0 +1,27 @@
+﻿using Blish_HUD.Controls;
+
+namespace TodoList.Components
+{
+    public sealed class TodoEntry : FlowPanel
+    {
+        private readonly Checkbox _checkbox;
+        
+        public TodoEntry()
+        {
+            FlowDirection = ControlFlowDirection.SingleLeftToRight;
+            WidthSizingMode = SizingMode.Fill;
+
+            _checkbox = new Checkbox
+            {
+                Parent = this, 
+                Text = "Test it like beckham",
+            };
+        }
+
+        protected override void DisposeControl()
+        {
+            _checkbox.Dispose();
+            base.DisposeControl();
+        }
+    }
+}
--- Source/Components/TodoScrollView.cs
+++ Source/Components/TodoScrollView.cs
@@ -1,24 +1,35 @@
-﻿using Blish_HUD.Controls;
+﻿using System.Collections.Generic;
+using Blish_HUD.Controls;
 using Microsoft.Xna.Framework;
 
 namespace TodoList.Components
 {
     public sealed class TodoScrollView : FlowPanel
     {
+        private const int OUTER_PADDING = 15;
+        private const int INNER_PADDING = 5;
+        
+        private readonly List<TodoEntry> _entries = new List<TodoEntry>();
+        
         public TodoScrollView(Resources resources, Settings settings)
         {
             FlowDirection = ControlFlowDirection.SingleTopToBottom;
             WidthSizingMode = SizingMode.Fill;
             HeightSizingMode = SizingMode.Fill;
+            OuterControlPadding = new Vector2(OUTER_PADDING, OUTER_PADDING);
+            ControlPadding = new Vector2(INNER_PADDING, INNER_PADDING);
 
             for (var i = 0; i < 50; i++)
-            {
-                new Label()
-                {
-                    Parent = this,
-                    Text = $"Entry Number {i}"
-                };
-            }
+                _entries.Add(new TodoEntry { Parent = this });
+        }
+
+        protected override void DisposeControl()
+        {
+            foreach (var entry in _entries)
+                entry.Dispose();
+            _entries.Clear();
+
+            base.DisposeControl();
         }
     }
 }

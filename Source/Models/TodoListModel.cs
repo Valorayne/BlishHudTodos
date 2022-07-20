5f1fb9134addac2b127d83bd618aba3f815787d7
Valorayne <kanu.privat@gmail.com>
2022-07-20 17:01:19 +0200

    Cleaning up the TodoListModel

--- Source/Models/TodoListModel.cs
+++ Source/Models/TodoListModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blish_HUD.Modules.Managers;
@@ -8,11 +7,11 @@ using Todos.Source.Utils.Reactive;
 
 namespace Todos.Source.Models
 {
-    public class TodoListModel : IDisposable
+    public class TodoListModel : ModelBase
     {
         private readonly IVariable<List<TodoModel>> _allTodos; 
-        public IProperty<IReadOnlyList<TodoModel>> AllTodos => _allTodos;
         
+        public IProperty<IReadOnlyList<TodoModel>> AllTodos => _allTodos;
         public IProperty<IReadOnlyList<TodoModel>> VisibleTodos;
 
         public static async Task<TodoListModel> Initialize(DirectoriesManager manager)
@@ -23,8 +22,8 @@ namespace Todos.Source.Models
 
         private TodoListModel(List<TodoModel> sortedTodos)
         {
-            _allTodos = Variables.Transient(sortedTodos);
-            VisibleTodos = AllTodos.Select(all => all.Where(t => t.IsVisible.Value).ToList());
+            _allTodos = Add(Variables.Transient(sortedTodos));
+            VisibleTodos = Add(AllTodos.Select(all => all.Where(t => t.IsVisible.Value).ToList()));
 
             foreach (var todo in sortedTodos)
                 SetupTodo(todo);
@@ -32,9 +31,16 @@ namespace Todos.Source.Models
 
         private void SetupTodo(TodoModel todo)
         {
+            void SortAllTodos() => _allTodos.Value = AllTodos.Value.OrderBy(t => t.OrderIndex.Value).ToList();
+            todo.OrderIndex.Subscribe(this, _ => SortAllTodos());
+            todo.IsVisible.Subscribe(this, _ => SortAllTodos());
             todo.IsDeleted.Subscribe(this, _ => OnTodoDeleted(todo), false);
-            todo.OrderIndex.Subscribe(this, OnOrderChanged);
-            todo.IsVisible.Subscribe(this, OnVisibilityChanged);
+        }
+
+        private void OnTodoDeleted(TodoModel todo)
+        {
+            TearDownTodo(todo);
+            _allTodos.Value = AllTodos.Value.Where(t => t != todo).ToList();
         }
 
         private void TearDownTodo(TodoModel todo)
@@ -45,23 +51,13 @@ namespace Todos.Source.Models
             todo.Dispose();
         }
 
-        private void OnVisibilityChanged(bool isVisible)
-        {
-            _allTodos.Value = AllTodos.Value.OrderBy(t => t.OrderIndex.Value).ToList();
-        }
-
-        private void OnOrderChanged(long newValue)
-        {
-            _allTodos.Value = AllTodos.Value.OrderBy(t => t.OrderIndex.Value).ToList();
-        }
-
         public void AddNewTodo()
         {
             var todo = new TodoModel(new TodoJson(), true);
             SetupTodo(todo);
             _allTodos.Value = AllTodos.Value.Append(todo).ToList();
         }
-        
+
         public void MoveUp(TodoModel todo)
         {
             var todos = new List<TodoModel>(AllTodos.Value);
@@ -76,7 +72,7 @@ namespace Todos.Source.Models
             foreach (var update in newOrderIndexes)
                 update.Key.OrderIndex.Value = update.Value;
         }
-        
+
         public void MoveDown(TodoModel todo)
         {
             var todos = new List<TodoModel>(AllTodos.Value);
@@ -92,18 +88,10 @@ namespace Todos.Source.Models
                 update.Key.OrderIndex.Value = update.Value;
         }
 
-        private void OnTodoDeleted(TodoModel todo)
-        {
-            TearDownTodo(todo);
-            _allTodos.Value = AllTodos.Value.Where(t => t != todo).ToList();
-        }
-
-        public void Dispose()
+        protected override void DisposeModel()
         {
             foreach (var todo in AllTodos.Value)
                 TearDownTodo(todo);
-            AllTodos.Dispose();
-            VisibleTodos.Dispose();
         }
     }
 }

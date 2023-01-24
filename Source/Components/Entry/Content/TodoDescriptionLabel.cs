295852588912cf46a4e8e4fd4909d90053644928
Valorayne <kanu.privat@gmail.com>
2023-01-24 12:20:24 +0100

    fix: PSNA Reset now also works with the "Copy to Clipboard" function

--- Source/Components/Entry/Content/TodoDescriptionLabel.cs
+++ Source/Components/Entry/Content/TodoDescriptionLabel.cs
@@ -1,10 +1,12 @@
-﻿using AsyncWindowsClipboard;
+﻿using System;
+using AsyncWindowsClipboard;
 using Blish_HUD.Controls;
 using Blish_HUD.Input;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Xna.Framework;
 using Todos.Source.Components.Generic;
 using Todos.Source.Models;
+using Todos.Source.Utils.Reactive;
 
 namespace Todos.Source.Components.Entry.Content
 {
@@ -12,6 +14,7 @@ namespace Todos.Source.Components.Entry.Content
     {
         private readonly WindowsClipboardService _clipboardService;
         private readonly TodoModel _todo;
+        private readonly IDisposable _tooltip;
 
         public TodoDescriptionLabel(TodoModel todo)
         {
@@ -22,7 +25,8 @@ namespace Todos.Source.Components.Entry.Content
             Location = new Point(0, 8);
 
             todo.Description.Subscribe(this, v => Text = v);
-            todo.ClipboardContent.Subscribe(this, v => BasicTooltipText = GetTooltip(v));
+            _tooltip = todo.Schedule.ClipboardContent.CombineWith(todo.ClipboardContent, (a, b) => a ?? b)
+                .Subscribe(this, v => BasicTooltipText = GetTooltip(v));
             todo.IsEditing.Subscribe(this, isEditing => Visible = !isEditing);
         }
 
@@ -33,9 +37,9 @@ namespace Todos.Source.Components.Entry.Content
 
         protected override void OnClick(MouseEventArgs e)
         {
-            var clipboardContent = _todo.ClipboardContent.Value ?? _todo.Schedule.ClipboardContent.Value;
+            var clipboardContent = _todo.Schedule.ClipboardContent.Value ?? _todo.ClipboardContent.Value;
             if (!clipboardContent.IsNullOrEmpty())
-                _clipboardService.SetTextAsync(_todo.Schedule.ClipboardContent.Value ?? _todo.ClipboardContent.Value)
+                _clipboardService.SetTextAsync(clipboardContent)
                     .ContinueWith(_ => TooltipNotification.Spawn("Content copied to clipboard!", e.MousePosition));
             base.OnClick(e);
         }
@@ -43,6 +47,7 @@ namespace Todos.Source.Components.Entry.Content
         protected override void DisposeControl()
         {
             _todo.Unsubscribe(this);
+            _tooltip.Dispose();
             base.DisposeControl();
         }
     }
